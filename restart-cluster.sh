243bc46170eee023fd48200dd01782242a1da602
Sebastian Florek <sebastian.florek@ts.fujitsu.com>
2018-03-02 03:49:05 -0800

    Update restart cluster script

--- restart-cluster.sh
+++ restart-cluster.sh
@@ -13,6 +13,7 @@ sudo sysctl net.bridge.bridge-nf-call-iptables=1
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
 # Copy kubeconfig file to $HOME/.kube/config
+mkdir -p $HOME/.kube
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown -R floreks $HOME/.kube
 
@@ -29,6 +30,9 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/s
 # Make Dashboard admin (for dev)
 kubectl create -f https://raw.githubusercontent.com/floreks/k8s-tools/master/example-yaml/dashboard-admin.yaml
 kubectl create -f https://raw.githubusercontent.com/floreks/k8s-tools/master/example-yaml/dashboard-admin-head.yaml
+
+# Add some test resources
+kubectl create -f https://raw.githubusercontent.com/floreks/k8s-tools/master/example-yaml/all-in-one.yaml
 
 # Start proxy and detach from console
 nohup kubectl proxy &
